e6d72201422060b24031d2725a30d429f1472e33
jiadchen <jiadchen@redhat.com>
2023-02-15 19:51:26 +0900

    テストケースを書く過程のケースをのこすようにした

--- backend/tests/unit-test/controllers/test_user_controller.py
+++ backend/tests/unit-test/controllers/test_user_controller.py
@@ -1,10 +1,54 @@
+import pytest
 from fastapi.testclient import TestClient
 from pytest_mock import MockFixture
 
 from backend.src.application.usecases.user_usecase import UserUsecase
 
+@pytest.mark.skip(reason="最初に書くテストケースです。usecaseを呼び出す前に書いたテストケースなので、usecaseを呼び出すと失敗してしまう")
+def test_ユーザと利用するカメラを登録するAPIをコールして正常時のResponseを返すこと(client: TestClient, mocker: MockFixture):
+    # Arrange
+    expected = {"message": "user and cameras are registered."}
+
+    # Act
+    response = client.post("/user", headers={"Content-Type": "application/json"}, json={
+        "name": "Tom",
+        "cameras": [
+            {"id": 1},
+            {"id": 2},
+            {"id": 3}
+        ]
+    })
+    actual = response.json()
+
+    # Assert
+    assert response.status_code == 200
+    assert actual == expected
+
+@pytest.mark.skip(reason="2番目に書くテストケースです。usecaseを呼び出す実装をうながすためのテストケース")
+def test_ユーザと利用するカメラを登録するAPIをコールして登録用のusecaseを呼び出すこと(client: TestClient, mocker: MockFixture):
+    # Arrange
+    # UserUsecaseクラスのメンバーメソッドをモック
+    user_usecase_mock = mocker.patch.object(UserUsecase, "register")
+
+    # Act
+    response = client.post("/user", headers={"Content-Type": "application/json"}, json={
+        "name": "Tom",
+        "cameras": [
+            {"id": 1},
+            {"id": 2},
+            {"id": 3}
+        ]
+    })
+
+    # Assert
+    assert response.status_code == 200
+    user_usecase_mock.assert_called_once()
 
 def test_ユーザと利用するカメラを登録するAPIをコールして登録用のusecaseを呼び出して正常時のResponseを返すこと(client: TestClient, mocker: MockFixture):
+    """
+    2番目のテストケースを書き終わった後、リファクタリング段階で、1番目と2番目のテストケースを一つにすることができることがわかり、
+    3番目のテストケースとして作成した
+    """
     # Arrange
     expected = {"message": "user and cameras are registered."}
     user_usecase_mock = mocker.patch.object(UserUsecase, "register")
