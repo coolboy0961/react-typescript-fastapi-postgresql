891f7db23a7f22f05f31cc63cd026dbca0a07d23
jiadchen <jiadchen@redhat.com>
2023-02-15 12:04:34 +0900

    エラーハンドラーを追加

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+import logging
+
 from src.interface.controllers import items_controller
 
+logger = logging.getLogger('uvicorn')
+
 app = FastAPI()
+
+
+@app.exception_handler(Exception)
+async def exception_handler(request: Request, exception: Exception):
+    logger.error("予期せぬエラーが発生しました。" + str(exception))
+    return JSONResponse(
+        status_code=500,
+        content={
+            "message": f"予期せぬエラーが発生しました。"
+        },
+    )
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
 
 app.include_router(items_controller.router)
--- backend/src/interface/controllers/items_controller.py
+++ backend/src/interface/controllers/items_controller.py
@@ -4,5 +4,6 @@ from fastapi import APIRouter
 router = APIRouter()
 
 @router.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int):
+    # return {"item_id": item_id, "name": name} # エラーハンドラーの動作確認をするため
+    return {"item_id": item_id}
