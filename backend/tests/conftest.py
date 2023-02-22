c0c1375474c9c19cdfce3e5d8b695e0dfe92a4dd
jiadchen <jiadchen@redhat.com>
2023-02-22 22:17:24 +0900

    client fixtureのやり方を変更

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,19 +1,8 @@
 import pytest
 from fastapi.testclient import TestClient
-from ..main import create_app
+from ..main import app
 
 
 @pytest.fixture()
-def app():
-    app = create_app()
-
-    # other setup can go here
-
-    yield app
-
-    # clean up / reset resources here
-
-
-@pytest.fixture()
-def client(app) -> TestClient:
-    return TestClient(create_app())
+def client() -> TestClient:
+    return TestClient(app)
