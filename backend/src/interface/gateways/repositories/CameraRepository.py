eb9ce4781461743cbc0ef0519f6f8d0397cdb741
jiadchen <jiadchen@redhat.com>
2023-02-27 18:19:44 +0900

    CameraRepositoryを実装した

--- backend/src/interface/gateways/repositories/CameraRepository.py
+++ backend/src/interface/gateways/repositories/CameraRepository.py
@@ -1,9 +1,14 @@
-
-
-
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from src.domain.entities.CameraEntity import CameraEntity
+from src.infrastructure.database import get_db
 from src.interface.gateways.repositories.models.CameraModel import CameraModel
+from src.domain.entities.UserEntity import UserEntity
 
 
 class CameraRepository:
-    def add(self, camera: CameraModel) -> None:
-        raise NotImplementedError
+    def add(self, camera: CameraEntity, user: UserEntity, db: Session = Depends(get_db)) -> None:
+        db_camera = CameraModel(id=camera.id, user_id=user.id)
+        db.add(db_camera)
+        db.commit()
+        db.refresh(db_camera)
---
+++ backend/tests/unit-test/gateways/repositories/test_camera_repository.py
@@ -0,0 +1,24 @@
+import pytest
+from sqlalchemy.orm import Session
+from src.domain.entities.CameraEntity import CameraEntity
+from src.domain.entities.UserEntity import UserEntity
+from src.interface.gateways.repositories.CameraRepository import CameraRepository
+from src.interface.gateways.repositories.models.CameraModel import CameraModel
+from fastapi.encoders import jsonable_encoder
+
+
+def test_カメラを登録するリポジトリをコールしてカメラ情報が登録されること(unit_test_db: Session):
+    # Arrange
+    expected_camera_model = CameraModel(id=1, user_id=1)
+    
+    # Act
+    target = CameraRepository()
+    camera_entity = CameraEntity(1, 0)
+    user_entity = UserEntity(1, "Tom")
+    target.add(camera_entity, user_entity, unit_test_db)
+    
+    actual_camera_modal = unit_test_db.query(CameraModel).first()
+    
+     # Assert
+    assert jsonable_encoder(
+        expected_camera_model) == jsonable_encoder(actual_camera_modal)
