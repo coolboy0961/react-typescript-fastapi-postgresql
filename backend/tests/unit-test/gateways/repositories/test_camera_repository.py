dfd7a7acd3b0610d32c791247883edf4fa1e253e
jiadchen <jiadchen@redhat.com>
2023-02-27 18:21:45 +0900

    リファクタリング

--- backend/tests/unit-test/gateways/repositories/test_camera_repository.py
+++ backend/tests/unit-test/gateways/repositories/test_camera_repository.py
@@ -10,15 +10,15 @@ from fastapi.encoders import jsonable_encoder
 def test_カメラを登録するリポジトリをコールしてカメラ情報が登録されること(unit_test_db: Session):
     # Arrange
     expected_camera_model = CameraModel(id=1, user_id=1)
-    
+
     # Act
     target = CameraRepository()
     camera_entity = CameraEntity(1, 0)
     user_entity = UserEntity(1, "Tom")
     target.add(camera_entity, user_entity, unit_test_db)
-    
+
     actual_camera_modal = unit_test_db.query(CameraModel).first()
-    
-     # Assert
+
+    # Assert
     assert jsonable_encoder(
         expected_camera_model) == jsonable_encoder(actual_camera_modal)
