44e5aca9993b80395feafe64f7c61366d36f561c
jiadchen <jiadchen@redhat.com>
2023-02-27 17:31:34 +0900

    conftestをリファクタリング

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -6,24 +6,19 @@ from sqlalchemy.orm import sessionmaker
 from src.infrastructure.database import Base
 from ..main import app
 
-# Create the new database session
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./tests/unit-test/test.db"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine)
-
 
 @pytest.fixture()
 def unit_test_db():
-
-    # Create the database
+    # Create the new database session
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./tests/unit-test/test.db"
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    TestingSessionLocal = sessionmaker(
+        autocommit=False, autoflush=False, bind=engine)
 
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
 
+    # Create the database
     db = TestingSessionLocal()
     try:
         yield db
