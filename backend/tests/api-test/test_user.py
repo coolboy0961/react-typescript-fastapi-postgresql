8190257ef3ca7809d45663e53cc4e1b3b356c599
jiadchen <jiadchen@redhat.com>
2023-02-27 19:26:39 +0900

    APIテストを実行できるようにした

---
+++ backend/tests/api-test/test_user.py
@@ -0,0 +1,22 @@
+import requests
+
+
+def test_ユーザと利用するカメラを登録するAPIをコールして正常時のResponseを返すこと():
+    # Arrange
+    expected = {"message": "user and cameras are registered."}
+
+    # Act
+    response = requests.post("http://localhost:8000/user", headers={"Content-Type": "application/json"}, json={
+        "id": 1,
+        "name": "Tom",
+        "cameras": [
+            {"id": 1},
+            {"id": 2},
+            {"id": 3}
+        ]
+    })
+    actual = response.json()
+
+    # Assert
+    assert response.status_code == 200
+    assert actual == expected
