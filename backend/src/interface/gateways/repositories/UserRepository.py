50d9224d8ee987355a22a7426e38d7d91cfc0fcb
jiadchen <jiadchen@redhat.com>
2023-02-27 17:02:33 +0900

    DBのテストが通った

--- backend/src/interface/gateways/repositories/UserRepository.py
+++ backend/src/interface/gateways/repositories/UserRepository.py
@@ -1,9 +1,13 @@
-
-
-
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from src.interface.gateways.repositories.models.UserModel import UserModel
+from src.infrastructure.database import get_db
 from src.domain.entities.UserEntity import UserEntity
 
 
 class UserRepository:
-    def add(self, user: UserEntity) -> None:
-        raise NotImplementedError
+    def add(self, user: UserEntity, db: Session = Depends(get_db)) -> None:
+        db_user = UserModel(id=user.id, name=user.name)
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
--- backend/tests/unit-test/gateways/repositories/test_user_repository.py
+++ backend/tests/unit-test/gateways/repositories/test_user_repository.py
@@ -6,23 +6,19 @@ from src.interface.gateways.repositories.models.UserModel import UserModel
 from src.interface.gateways.repositories.UserRepository import UserRepository
 
 
-
 # @pytest.mark.skip("未完成")
-
 
 def test_ユーザを登録するリポジトリをコールしてユーザ情報が登録されること(unit_test_db, mocker: MockFixture):
     # Arrange
-    excepted_user = UserEntity(1, "Tom")
+    excepted_user_model = UserModel(id=1, name="Tom")
 
     # Act
     target = UserRepository()
-    # target.add(excepted_user)
-    db_user = UserModel(id=1, name="Tom")
-    unit_test_db.add(db_user)
-    unit_test_db.commit()
-    unit_test_db.refresh(db_user)
+    user_entity = UserEntity(
+        excepted_user_model.id, excepted_user_model.name)
+    target.add(user_entity, unit_test_db)
 
-    actual_user = unit_test_db.query(UserModel)
+    actual_user_modal = unit_test_db.query(UserModel).first()
 
     # Assert
-    assert excepted_user == actual_user
+    assert excepted_user_model.__eq__(actual_user_modal)
