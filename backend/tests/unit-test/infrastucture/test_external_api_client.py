2c6e8e8d191d6ecdd9a6d1f6d6e48ee89019e237
jiadchen <jiadchen@redhat.com>
2023-03-01 20:30:02 +0900

    ExternalApiClientの単体テストを追加した

---
+++ backend/tests/unit-test/infrastucture/test_external_api_client.py
@@ -0,0 +1,20 @@
+from src.infrastructure.ExternalApiClient import ExternalApiClient
+
+def test_ExternalApiClientのgetメソッドで情報を取得することができること(requests_mock):
+    # Arrange
+    expected = {
+        "id": 1,
+        "message": "this is a get test."
+    }
+
+    requests_mock.get("http://localhost:1234/api/get-test", json={
+        "id": 1,
+        "message": "this is a get test."
+    })
+    
+    # Act
+    target = ExternalApiClient()
+    actual = target.get("/get-test").json()
+    
+    # Assert
+    assert expected == actual
