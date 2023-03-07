8333585fca463054eb7d5bbf9c8bf3e3e2b631cb
jiadchen <jiadchen@redhat.com>
2023-03-07 13:49:50 +0900

    コントローラーの異常系を追加した

--- backend/tests/unit-test/controllers/test_traffic_controller.py
+++ backend/tests/unit-test/controllers/test_traffic_controller.py
@@ -4,6 +4,7 @@ from src.domain.entities.CameraEntity import CameraEntity
 from src.domain.entities.UserEntity import UserEntity
 from src.application.usecases.TrafficUsecase import TrafficUsecase
 
+
 def test_交通量検索APIを呼び出して正常時のResponseを取得できること(client: TestClient, mocker: MockFixture):
 
     # Arrange
@@ -30,13 +31,40 @@ def test_交通量検索APIを呼び出して正常時のResponseを取得でき
         CameraEntity(2, 67),
         CameraEntity(3, 19),
     ]
-    traffic_usercase_mock = mocker.patch.object(TrafficUsecase, "get_traffic", return_value=[user, cameras])
+    traffic_usercase_mock = mocker.patch.object(
+        TrafficUsecase, "get_traffic", return_value=[user, cameras])
 
     # Act
     response = client.get("/api/v1/users/1/traffic")
-    actual= response.json()
+    actual = response.json()
 
     # Assert
     assert response.status_code == 200
     assert actual == expected
     traffic_usercase_mock.assert_called_once_with(1)
+
+
+def test_交通量検索APIを呼び出してuser_idがintではない場合正しいエラーレスポンスを返すこと(client: TestClient, mocker: MockFixture):
+    # Arrange
+    expected = {
+        "error_code": "SP422001",
+        "message": "APIリクエストのパラメータチェックが失敗しました。",
+        "detail": [{"loc": ["path", "user_id"], "msg": "value is not a valid integer", "type": "type_error.integer"}]
+    }
+    user = UserEntity(1, "Tom")
+    cameras = [
+        CameraEntity(1, 35),
+        CameraEntity(2, 67),
+        CameraEntity(3, 19),
+    ]
+    traffic_usercase_mock = mocker.patch.object(
+        TrafficUsecase, "get_traffic", return_value=[user, cameras])
+
+    # Act
+    response = client.get("/api/v1/users/abc/traffic")
+    actual = response.json()
+
+    # Assert
+    assert response.status_code == 422
+    assert actual == expected
+    traffic_usercase_mock.assert_not_called()
