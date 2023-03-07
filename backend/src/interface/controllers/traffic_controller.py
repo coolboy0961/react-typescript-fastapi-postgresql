aa572c5ae64751b7608af1b2ab31808670a357c5
jiadchen <jiadchen@redhat.com>
2023-03-07 12:17:13 +0900

    コメント追加

--- backend/src/interface/controllers/traffic_controller.py
+++ backend/src/interface/controllers/traffic_controller.py
@@ -7,8 +7,10 @@ router = APIRouter(prefix="/api")
 # traffic情報はuser_idによって取得され、usersと関連があるため、パスを"/v1/users/{user_id}/traffic"の形にした
 @router.get("/v1/users/{user_id}/traffic")
 def get_traffic(user_id: int):
+    # get_trafficメソッドの戻り値はuserとcamerasのタプルであるため、userとcamerasにそれぞれ代入している
     user, cameras = TrafficUsecase().get_traffic(user_id)
     return {
         "name": user.name,
+        # この部分はリスト内包表記で書かれており、camerasの中身を一つずつ辞書に変換してリストにしている
         "cameras": [{"id": camera.id, "count": camera.count} for camera in cameras],
     }
